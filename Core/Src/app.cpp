1accafa82e910aedc856d5c6f4cc043e6018867e
Sylvain Garcia <garcia.6l20@gmail.com>
2022-03-22 09:07:47 +0100

    add simple gpio class

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -16,6 +16,27 @@ extern "C" {
 #include <main.h>
 }
 
+namespace stm32 {
+class gpio {
+public:
+	GPIO_TypeDef *port_;
+	uint16_t pin_;
+
+	void toogle() {
+		HAL_GPIO_TogglePin(port_, pin_);
+	}
+
+	void operator=(bool on) {
+		HAL_GPIO_WritePin(port_, pin_, on ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	}
+
+	operator bool() {
+		return HAL_GPIO_ReadPin(port_, pin_) == GPIO_PIN_SET;
+	}
+};
+}
+
+
 using unifex::task;
 using unifex::when_all;
 using unifex::schedule_at;
@@ -30,18 +51,24 @@ extern "C" int application(void) {
 	unifex::stm32_bare_context ctx{};
     auto scheduler = ctx.get_scheduler();
 
-    auto led_toggler = [&scheduler](auto port, auto pin, auto delay) -> task<void> {
+    auto led_toggler = [&scheduler](stm32::gpio gpio, auto delay) -> task<void> {
 		while (true) {
 			co_await schedule_at(scheduler, now(scheduler) + delay);
-			HAL_GPIO_TogglePin(port, pin);
+			gpio.toogle();
 		}
-		co_return;
     };
 
     sync_wait(when_all(
-		led_toggler(LD1_GPIO_Port, LD1_Pin, 500ms),
-		led_toggler(LD2_GPIO_Port, LD2_Pin, 1s),
-		led_toggler(LD3_GPIO_Port, LD3_Pin, 2s),
+		[&]() -> task<void> {
+    		auto user_btn = stm32::gpio{USER_Btn_GPIO_Port, USER_Btn_Pin};
+    		auto led = stm32::gpio{LD1_GPIO_Port, LD1_Pin};
+    		while (true) {
+    			led = bool(user_btn);
+    			co_await schedule_at(scheduler, now(scheduler) + 250ms);
+    		}
+    	}(),
+		led_toggler({LD2_GPIO_Port, LD2_Pin}, 1s),
+		led_toggler({LD3_GPIO_Port, LD3_Pin}, 2s),
 		[&]() -> task<void> {
 
 			ctx.run();
